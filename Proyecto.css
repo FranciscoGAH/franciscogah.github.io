40bc70f1f10fc58c54faddecd759e6d26067c488
FranciscoGAH <117939430+FranciscoGAH@users.noreply.github.com>
2022-12-13 21:53:22 -0600

    Create Proyecto.css

---
+++ Proyecto.css
@@ -0,0 +1,302 @@
+body 
+{
+  font-family: 'Rosarivo', serif;
+    background: #090d00;
+    color: rgba(255, 255, 255, 0.25);
+    text-align: center;
+    margin: 0;
+  }
+  
+  p {
+    text-transform: uppercase;
+    letter-spacing: 0.5em;
+    display: inline-block;
+    border: 4px double rgba(255, 255, 255, 0.25);
+    border-width: 4px 0;
+    padding: 1.5em 0em;
+    position: absolute;
+    top: 2%;
+    left: 50%;
+    width: 40em;
+    margin: 0 0 0 -20em;
+  }
+  p span {
+    font: 700 4em/1 "Oswald", sans-serif;
+    letter-spacing: 0;
+    padding: 0.25em 0 0.325em;
+    display: block;
+    margin: 0 auto;
+    text-shadow: 0 0 80px rgba(255, 255, 255, 0.5);
+    
+    background: url(https://i.ibb.co/RDTnNrT/animated-text-fill.png) repeat-y;
+    -webkit-background-clip: text;
+    background-clip: text;
+    
+    -webkit-text-fill-color: transparent;
+    -webkit-animation: aitf 80s linear infinite;
+    
+    -webkit-transform: translate3d(0, 0, 0);
+    -webkit-backface-visibility: hidden;
+  }
+  
+ 
+  @-webkit-keyframes aitf {
+    0% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 100% 50%;
+    }
+  }
+
+aside
+{
+    float: right; 
+    border: 4px double rgba(255, 255, 255, 0.25);
+    width: 15%;
+    min-height: 400px;
+    padding: 3px 3px;
+}
+
+h3
+{
+text-align: left;  
+}
+
+h4
+{
+text-align: justify;
+font-size: larger;
+margin-right: 400px;
+margin-left: 160px;
+}
+
+#centrar
+{
+margin-left: 320px;
+height: 410PX;
+width: 750px;
+}
+
+h5
+{
+margin-right: 80%;
+font-size: x-large;
+}
+
+#derecha
+{
+height: 300px;
+float: right;
+margin-right: 10px;
+}
+
+h6
+{
+margin-left: 50px;
+margin-right: 680px;
+text-align: justify;
+font-size: larger;  
+}
+
+h1
+{
+  margin-left: 80%;
+  font-size: x-large;
+  color: rgb(17, 83, 17);
+}
+
+#izquierda
+{
+  height: 320px;
+  float: left;
+  margin-right: 10px;
+}
+
+#carnivoros
+{
+color: rgb(102, 30, 30);
+}
+
+#hervivoros
+{
+  margin-left: 590px;
+  margin-right: 30px;
+  text-align: justify;
+  font-size: larger;
+}
+
+#omnivoro
+{
+  margin-left: 30px;
+  margin-right: 630px;
+  text-align: justify;
+  font-size: larger;
+}
+
+#omnivoros
+{
+color: rgb(114, 94, 70);
+}
+
+#om
+{
+  width: 600px;
+  height: 300px;
+  float: right;
+  margin-right: 10px;
+}
+
+a {
+  color: rgb(55, 76, 134);
+  background-color: rgb(0, 0, 0);
+  transition: background-color .5s;
+}
+
+a:hover 
+{
+background-color: rgba(255, 255, 255, 0.651);
+}
+
+a:active
+{
+color:rgb(255, 0, 0)
+}
+
+.menu,.menu ul,.menu li,.menu a{border:none;outline:none;margin:0;padding:0;z-index:999}
+.menu{height:40px;width:auto; background-color: #222;background-image: -webkit-gradient(linear, left top, left bottom, from(#555), to(#222));background-image: -webkit-linear-gradient(top, #555, #222);background-image: -moz-linear-gradient(top, #555, #222);background-image: -ms-linear-gradient(top, #555, #222);background-image: -o-linear-gradient(top, #555, #222);background-image: linear-gradient(top, #555, #222); -moz-box-shadow: 0 5px 5px #000;-webkit-box-shadow: 0 5px 5px #000;box-shadow: 0 5px 5px #000;  }
+.menu li{position:relative;list-style:none;float:left;display:block;height:40px}
+.menu li a{background:url(https://4.bp.blogspot.com/-n7OkwgMuP7Q/UQs19HkuUsI/AAAAAAAABiQ/k3qWwdmqgfQ/s1600/bagi.gif) no-repeat top right;display:block;text-decoration:none;font-family:Arial;font-weight:700;font-size:14px;color:#ddd;text-shadow:1px 1px 1px #000;-webkit-transition:color .2s ease-in-out;-moz-transition:color .2s ease-in-out;-o-transition:color .2s ease-in-out;-ms-transition:color .2s ease-in-out;transition:color .2s ease-in-out;margin:0;padding:12px 14px  12px}
+.menu li:first-child a{padding:10px 10px 7px}
+.menu li:hover > a{color:#48d}
+.menu ul{position:absolute;top:41px;left:0;opacity:0;background:#fdfdfd url(https://4.bp.blogspot.com/-mlmoMuUf48g/UQu3dezXeFI/AAAAAAAABpE/cDDiKShrEu0/s1600/menuhover.png) repeat-x top;-webkit-border-radius:0 0 5px 5px;-moz-border-radius:0 0 5px 5px;border-radius:0 0 5px 5px;border:1px solid #666;border-top:none;-webkit-transition:opacity .25s ease .1s;-moz-transition:opacity .25s ease .1s;-o-transition:opacity .25s ease .1s;-ms-transition:opacity .25s ease .1s;transition:opacity .25s ease .1s}
+.menu li:hover > ul{opacity:1}
+.menu ul li{height:0;overflow:hidden;-webkit-transition:height .25s ease .1s;-moz-transition:height .25s ease .1s;-o-transition:height .25s ease .1s;-ms-transition:height .25s ease .1s;transition:height .25s ease .1s;padding:0}
+.menu li:hover > ul li{height:36px;overflow:visible;padding:0}
+.menu ul li a{color:#111;font-weight:400;width:100px;margin:0;padding:10px 0 10px 30px;text-shadow:none}
+.menu ul li:first-child a{padding:10px 14px  10px 30px}
+.menu ul li:last-child a{border:none}
+.menu a.trigger{background:url(https://3.bp.blogspot.com/-pJAQN5knuO8/UQre-F12FjI/AAAAAAAABgs/6WarLD7FwNw/s1600/arrow.png) no-repeat 6px center}
+
+
+
+/**hover**/
+.top li:nth-child(1):hover ~ #lavalamp {
+left: 17px;
+}
+.top li:nth-child(2):hover ~ #lavalamp {
+left: 26px;
+}
+.top li:nth-child(3):hover ~ #lavalamp {
+left: 330px;
+}
+.top li:nth-child(4):hover ~ #lavalamp {
+left: 4px;
+}
+#top li:nth-child(5):hover ~ #lavalamp {
+left: 340px;
+}
+#top li:nth-child(6):hover ~ #lavalamp {
+left: 420px;
+}
+#lavalamp {
+background: url("https://lh5.googleusercontent.com/-8bNrjIsGufg/UPHpz7uAMDI/AAAAAAAAESw/tdbx5M94lBo/s1600/bg-light.png") no-repeat scroll center top transparent;
+display: block;
+height: 62px;
+left: 17px;
+position: absolute;
+top: -27px;
+width: 124px;
+z-index: 10;
+}
+#top span {
+background: none repeat scroll 0 0 rgba(0, 126, 255, 0.25);
+border: 1px solid rgba(0, 0, 0, 0.7);
+border-radius: 3px 3px 3px 3px;
+box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.1) inset;
+font-size: 11px;
+line-height: 1;
+padding: 4px 8px;
+position: absolute;
+right: -10px;
+top: -10px;
+z-index: 50;
+}
+
+
+
+
+.navtime{width:300px;float:right;margin:0 auto;padding:0 auto;font-size:8px Arial;}
+
+
+
+/* Redes sociales */
+#buttonbg {width: 260px;margin: 10px 20px;padding: 0;}
+#buttonbg li {cursor: pointer;color:#fff;height: 48px;position: relative;list-style-type: none;  }
+#buttonbg .icon {background: #D91E76 url('https://2.bp.blogspot.com/-JKB0aVYaIW0/UC-oBl8NXxI/AAAAAAAAEYA/IbgyikdkM5A/s1600/blsocial12.png') 0 0 no-repeat;
+background-color: rgba(217, 30, 118, .42);border-radius: 30px;display: block;
+color: #fff;float: none;height: 48px;line-height: 48px;
+margin: 5px 0;position: relative;text-align: left;text-indent: 90px;
+text-shadow: #333 0 1px 0;white-space: nowrap;width: 48px;z-index: 5;
+-webkit-transition: width .25s ease-in-out, background-color .25s ease-in-out;
+-moz-transition: width .25s ease-in-out, background-color .25s ease-in-out;
+-o-transition: width .25s ease-in-out, background-color .25s ease-in-out;
+transition: width .25s ease-in-out, background-color .25s ease-in-out;
+-webkit-box-shadow: rgba(0, 0, 0, .28) 0 2px 3px;
+-moz-box-shadow: rgba(0,0,0, .28) 0 2px 3px;
+-o-box-shadow: rgba(0,0,0, .28) 0 2px 3px;
+box-shadow: rgba(0, 0, 0, .28) 0 2px 3px;
+text-decoration: none;}
+#buttonbg span:hover {visibility: hidden;}
+#buttonbg span {display: block;top: 15px;position: absolute;left: 90px;}
+#buttonbg .icon {color: #fafafa;overflow: hidden;}
+#buttonbg .fb {background-color: rgba(45,118,185, .42); background-position: 0 -382px;}
+#buttonbg .twit {background-color: rgba(0, 161, 223, .42); background-position: 0 -430px;}
+#buttonbg .pint {background-color: rgba(204, 0, 0, .42);background-position: 0 -526px;}
+#buttonbg .ytube {background-color: rgba(170, 0, 0, .42); background-position: 0 -670px;}
+#buttonbg li:hover .icon {width: 250px; }
+#buttonbg li:hover .icon {background-color: #d91e76; }
+#buttonbg li:hover .fb {background-color: #2d76b9;background-position: 0 2px;}
+#buttonbg li:hover .twit {background-color: #00A1DF; background-position: 0 -46px; }
+#buttonbg li:hover .pint {background-color: #C00;background-position: 0 -142px; }
+#buttonbg li:hover .ytube {background-color: #A00;background-position: 0 -286px; }
+#buttonbg .icon:active {bottom: -2px;-webkit-box-shadow: none;-moz-box-shadow: none;-o-box-shadow: none;}
+
+table{
+  border-color: rgba(21, 250, 0, 0.226);
+  border-width: 5px;
+  border-style: groove;
+  background-color: rgba(0, 0, 0, 0.25);
+  font-size: 18px;
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+thead{
+  background-color: rgb(255, 156, 27);
+  font-weight: bolder;
+  text-shadow: 1px 1px 1px rgb(7, 159, 30);
+}
+
+#rextabla
+{
+width: 210px;
+height: 150px;
+}
+
+#espinosauriotable
+{
+  width: 210px;
+  height: 150px;
+}
+
+#raptortabla
+{
+  width: 210px;
+  height: 150px;
+}
+
+tr
+{
+text-align: center;
+}
+
